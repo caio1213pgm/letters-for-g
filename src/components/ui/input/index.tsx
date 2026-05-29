3cc4c4d5d1d15baf45be05559b299da5b60a9b4c
caio1213pgm <caiofxpgm@gmail.com>
2026-05-28 21:41:28 -0300

    feat: implement showPassword to vies password

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -1,12 +1,16 @@
 "use client";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import { Button } from "../button";
 import { Label } from "../label";
 import { InputDefault } from "./input";
 
 export type InputProps = {
   placeholder?: string;
-  label?: string;
+  label: string;
   value?: string | number;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: "text" | "password";
 } & React.ComponentProps<"input">;
 
 export function Input({
@@ -14,17 +18,36 @@ export function Input({
   label,
   value,
   onChange,
+  type,
   ...props
 }: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="flex flex-col gap-1">
       <Label>{label}</Label>
-      <InputDefault
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        {...props}
-      />
+      <div className="flex">
+        <InputDefault
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          type={showPassword ? "text" : type}
+          {...props}
+        />
+        {type === "password" && (
+          <Button
+            type="button"
+            variant="ghost"
+            className="hover:bg-transparent"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {!showPassword ? (
+              <EyeOff className="h-4 w-4 text-zinc-500" />
+            ) : (
+              <Eye className="h-4 w-4 text-zinc-500" />
+            )}
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
