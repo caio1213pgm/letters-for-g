26573904f737f53ae8e9f750f3997b96d66d3584
caio1213pgm <caiofxpgm@gmail.com>
2026-07-02 21:19:30 -0300

    feat: implement heading buttons 1 - 3

--- src/components/editor/editorControls.tsx
+++ src/components/editor/editorControls.tsx
@@ -2,6 +2,9 @@
 import { Editor } from "@tiptap/react";
 import {
   Bold,
+  Heading1,
+  Heading2,
+  Heading3,
   Italic,
   RemoveFormatting,
   Strikethrough,
@@ -16,6 +19,30 @@ export default function EditorControls({ editor }: EditorControlsProps) {
   return (
     <div className="w-full">
       <div className="flex gap-2 items-center">
+        <Button
+          variant="outline"
+          onClick={() => {
+            editor?.chain().focus().toggleHeading({ level: 1 }).run();
+          }}
+        >
+          <Heading1 />
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            editor?.chain().focus().toggleHeading({ level: 2 }).run();
+          }}
+        >
+          <Heading2 />
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            editor?.chain().focus().toggleHeading({ level: 3 }).run();
+          }}
+        >
+          <Heading3 />
+        </Button>
         <Button
           variant="outline"
           onClick={() => {
@@ -37,6 +64,7 @@ export default function EditorControls({ editor }: EditorControlsProps) {
           variant="outline"
           onClick={() => {
             editor?.chain().focus().toggleStrike().run();
+            console.log(editor?.getHTML());
           }}
         >
           <Strikethrough />
@@ -53,6 +81,7 @@ export default function EditorControls({ editor }: EditorControlsProps) {
           variant="outline"
           onClick={() => {
             editor?.chain().focus().unsetAllMarks().run();
+            editor?.chain().focus().clearNodes().run();
           }}
         >
           <RemoveFormatting />
